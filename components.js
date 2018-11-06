f2f901e7c7c519e27b3b016a294cd9e506ac6e02
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-11-06 12:18:47 +0100

    add arrows and prepare the infinite-rotate-on-hover component.

--- components.js
+++ components.js
@@ -15,3 +15,33 @@ AFRAME.registerComponent('click-destroy-helico', {
         });
     }
 });
+
+AFRAME.registerComponent('infinite-rotate-on-hover', {
+    // if clicked?
+    init: function () {
+        console.log("init")
+        //add animation when element is "hovered"
+        this.el.addEventListener('mouseenter', function (evt) {
+            console.log("hover begun");
+            // helicoAnim = document.createElement('a-animation')
+            //
+            // helicoAnim.setAttribute("attribute", "position")
+            // helicoAnim.setAttribute("dur", "10000")
+            // helicoAnim.setAttribute("fill", "forwards")
+            // helicoAnim.setAttribute("to", "1 -10000 -3")
+            // document.querySelector("#helico").appendChild(helicoAnim)
+        });
+
+        //and remove it when it isn't
+        this.el.addEventListener('mouseleave', function (evt) {
+            console.log("hover lost");
+            // helicoAnim = document.createElement('a-animation')
+            //
+            // helicoAnim.setAttribute("attribute", "position")
+            // helicoAnim.setAttribute("dur", "10000")
+            // helicoAnim.setAttribute("fill", "forwards")
+            // helicoAnim.setAttribute("to", "1 -10000 -3")
+            // document.querySelector("#helico").appendChild(helicoAnim)
+        });
+    }
+});
